c2d116bc1d5bac12af09049a66a4ed89694d5f03
yuce1 <724739722@qq.com>
2022-05-21 10:13:35 +0800

    添加测试文件
    测试文件中可以进行测试

---
+++ code/agent/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+
+server_usage = 0
+
+# 获取整台服务器的利用率
+def read_server_usage():
+	global server_usage
+	# 命令行中要运行的语句
+	cmd1 = "sar -P ALL -u 1 5"
+	# 执行以上命令，并且返回结果
+	textlist = os.popen(cmd1).readlines()
+	# 异常处理,读取到的文件应该总是一行，进行基本的判断
+	for text in textlist:
+		a = re.match(r'^Average:\s+all\s+\S+\s+',text)
+		if a:
+			temp_list = re.split(r'\s+', text)
+			server_usage = temp_list[2]
+			server_usage = 0
+	if server_usage == 0:
+		sys.stderr.write("[ERROR] 无法获取整台服务器的CPU利用率!\n")
+		exit(-1)
+
+if __name__ == "__main__":
+	read_server_usage()
