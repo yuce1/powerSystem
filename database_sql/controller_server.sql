f1235915ac0fd1f744944b0a33424cd146a775b2
yuce1 <724739722@qq.com>
2022-07-11 10:22:09 +0800

    Update controller_server.sql
    修改数据库文件

--- database_sql/controller_server.sql
+++ database_sql/controller_server.sql
@@ -39,25 +39,12 @@ CREATE TABLE `controller`  (
   `controller_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'controller主键id',
   `controller_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '控制器名称，信息描述',
   `server_machine_id` int(10) NOT NULL COMMENT '控制器所处的服务器id（外键）',
-  `controller_tdp` int(5) NOT NULL COMMENT '控制器的tdp的值',
+  `rack_info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '对应的机架信息',
   `is_del` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '是否删除(0:未删除；1:已删除)',
   `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`controller_id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Table structure for controller_server
--- ----------------------------
-DROP TABLE IF EXISTS `controller_server`;
-CREATE TABLE `controller_server`  (
-  `controller_server_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '控制器agent对应关系的主键',
-  `controller_server_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '控制器agent对应关系名称，信息描述',
-  `controller_id` int(10) NOT NULL COMMENT '控制器id（外键）',
-  `server_machine_id` int(10) NOT NULL COMMENT '服务器id（外键）',
-  `is_del` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '是否删除(0:未删除；1:已删除)',
-  `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  PRIMARY KEY (`controller_server_id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for server_machine
@@ -86,6 +73,39 @@ CREATE TABLE `server_machine_power`  (
   `is_del` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '是否删除(0:未删除；1:已删除)',
   `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`server_machine_power_id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `power_line`;
+CREATE TABLE `power_line`  (
+  `power_line_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '线路的唯一主键',
+  `power_line_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '线路名称，信息描述',
+  `power_line_tdp` int(5) NOT NULL COMMENT '线路的tdp的值',
+  `is_del` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '是否删除(0:未删除；1:已删除)',
+  `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`power_line_id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+
+DROP TABLE IF EXISTS `line_server`;
+CREATE TABLE `line_server`  (
+  `line_server_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '线路服务器供应关系id',
+  `line_server_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '线路服务器供应关系名称，信息描述',
+  `server_machine_id` int(10) NOT NULL COMMENT '服务器id（外键）',
+  `power_line_id` int(10) NOT NULL COMMENT '线路id（外键）',
+  `is_del` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '是否删除(0:未删除；1:已删除)',
+  `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`line_server_id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `line_controller`;
+CREATE TABLE `line_controller`  (
+  `line_controller_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '线路机架供应关系id',
+  `line_controller_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '线路机架供应关系名称，信息描述',
+  `controller_id` int(10) NOT NULL COMMENT '控制器id（外键）',
+  `power_line_id` int(10) NOT NULL COMMENT '线路id（外键）',
+  `is_del` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '是否删除(0:未删除；1:已删除)',
+  `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`line_controller_id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
